bae7b95f8176f17eab9baf35cb0c2ffec4b77cd3
Agam Jain <jainagam4@gmail.com>
2024-10-18 23:47:39 +0530

    Add File model to server

---
+++ server/models/File.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema({
+  path: {
+    type: String,
+    required: true,
+  },
+  originalName: {
+    type: String,
+    required: true,
+  },
+  password: String,
+  downloadCount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+const File = mongoose.model('File', fileSchema);
+
+export default File;
